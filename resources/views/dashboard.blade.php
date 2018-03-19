65a3ef8f484fb6ce7f0a15979e3311cbcf5bf154
Stevanno Hero Leadervand <eshael@Stevannos-Macbook-Pro.local>
2018-03-19 21:46:31 +0700

    fix dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,42 +5,39 @@
 @section('content')
     <div class="container-fluid">
       <div class="row">
-        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-          <div class="sidebar-sticky">
-            <ul class="nav flex-column">
-              <li class="nav-item">
-                <a class="nav-link active" href="/dashboard">
-                  <span data-feather="home"></span>
-                  Dashboard<span class="sr-only">(current)</span>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/members">
-                  <span data-feather="users"></span>
-                  Members List
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/dashboard/#">
-                  <span data-feather="file"></span>
-                  Articles
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-
-        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
-          <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
-            <h1 class="h2">Members List</h1>
-            <div class="btn-toolbar mb-2 mb-md-0">
-              <div class="btn-group mr-2">
-                <button class="btn btn-primary">Add Member</button>
-                <button class="btn btn-default">Upload CSV</button>
-              </div>
+        <nav class="col-3 sidebar">
+            <div class="sidebar-sticky">
+              <ul class="nav flex-column">
+                <li class="nav-item">
+                  <a class="nav-link active" href="/dashboard">
+                    <span data-feather="home"></span>
+                    <i class="sideMenu">Dashboard</i><span class="sr-only">(current)</span>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/members">
+                    <span data-feather="users"></span>
+                    <i class="sideMenu">Members List</i>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/dashboard/#">
+                    <span data-feather="file"></span>
+                    <i class="sideMenu">Articles</i>
+                  </a>
+                </li>
+              </ul>
             </div>
-          </div>
-        </main>
+          </nav>
+
+        <main role="main" class="col-7">
+            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+              <img src="{{URL::asset('storage/banner.jpg')}}" id="bannerMember">
+            </div>
+            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+              <h1 class="h2">What to Show</h1>
+            </div>
+          </main>
       </div>
     </div>
 
