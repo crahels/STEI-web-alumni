95dae5a1c28e2268429e17be3d3932a404972ffd
Winarto <13515061@std.stei.itb.ac.id>
2018-03-19 20:19:19 +0700

    Fix navbar bug & remove navbar admin content

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,7 +1,6 @@
-  <link rel="stylesheet" type="text/css" href="css/style.css">
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container">
-          <img src="res/logo_itb.png">
+          <img src="/storage/logo_itb.png">
           <a class="navbar-brand" href="/">Web Alumni STEI</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -13,9 +12,11 @@
                   <li class="nav-item">
                     <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
                   </li>
+                  @if(!Auth::guest() &&  Auth::user()->IsAdmin == 1)
                   <li class="nav-item">
                     <a class="nav-link" href="/members">Members</a>
                   </li>
+                  @endif
                   <li class="nav-item">
                     <a class="nav-link" href="#">TBD</a>
                   </li>
@@ -25,8 +26,8 @@
               <ul class="navbar-nav ml-auto navbar-right">
                   <!-- Authentication Links -->
                   @guest
-                      <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login Admin') }}</a></li>
-                      <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
+                      <!-- <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login Admin') }}</a></li> -->
+                      <!-- <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li> -->
                   @else
                       <li class="nav-item dropdown">
                           <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
         <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
         <title>@yield('title')</title>
 
