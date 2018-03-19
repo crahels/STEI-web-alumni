6f5111c0c69821fa14cc4b126ad58ccf18481a8a
Tasya <agathastellatasya@gmail.com>
2018-03-19 17:03:43 +0700

    Fixed title blade login and register for admin.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.app')
 
+@section('title', 'Login Admin')
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header">{{ __('Login Admin') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.app')
 
+@section('title', 'Register Admin')
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header">{{ __('Register Admin') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
