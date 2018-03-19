4d04cc9c208d23590e2460348a6957d02bbade0c
Stevanno Hero Leadervand <eshael@Stevannos-Macbook-Pro.local>
2018-03-19 21:41:34 +0700

    add banner

--- resources/views/members/list.blade.php
+++ resources/views/members/list.blade.php
@@ -32,6 +32,9 @@
   
           <main role="main" class="col-7">
             <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+              <img src="{{URL::asset('storage/banner.jpg')}}" id="bannerMember">
+            </div>
+            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
               <h1 class="h2">Members List</h1>
               <button class="btn btn-primary">Add Member</button>
             </div>
