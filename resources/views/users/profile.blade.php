2ab7142a5dc3894fd755c506ec4d6ff1ea664838
stevannohero <13515082@std.stei.itb.ac.id>
2018-04-04 21:50:11 +0700

    add profile info

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -13,7 +13,81 @@
     <div class="profile-content">
       <h1 id="profile-header"> MY PROFILE </h1>
       <div class="profile-info">
-          
+          <div class=" col-md-9 col-lg-9 "> 
+                                <table class="table table-user-information">
+                                    <tbody>
+                                        <tr>
+                                            <td>Email</td>
+                                            <td><a href="mailto:{{$user->email}}">{{$user->email}}</a></td>
+                                        </tr>
+                                            <td>Phone Number</td>
+                                            <td>{{$user->phone_number}}                            
+                                        </tr>
+                                        <tr>
+                                            <td>Company</td>
+                                            <td>{{$user->company}}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Interest</td>
+                                            <td>{{$user->interest}}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Address</td>
+                                            <td>{{$user->address}}</td>
+                                        </tr>
+                                        @if(Auth::guard('member')->user() != null && Auth::guard('member')->user()->id == $user->id)
+                                            @if(Auth::guard('member')->user()->facebook_email != null)
+                                                <tr>
+                                                    <td>
+                                                        Facebook Account
+                                                    </td>
+                                                    <td>
+                                                        <a href="/link/facebook/delete" data-original-title="Delete Facebook Link" 
+                                                            data-toggle="tooltip" type="button" class="btn btn-sm btn-danger">
+                                                            {{Auth::guard('member')->user()->facebook_email}} <i class="glyphicon glyphicon-remove"></i> 
+                                                        </a>
+                                                    </td>
+                                                </tr>
+                                            @else
+                                                <tr>
+                                                    <td>
+                                                        Facebook Account
+                                                    </td>
+                                                    <td>
+                                                        <a href="/link/facebook" data-toggle="tooltip" type="button" class="btn btn-sm btn-success">
+                                                            Link facebook account
+                                                        </a>
+                                                    </td>
+                                                </tr>
+                                            @endif
+                                            @if(Auth::guard('member')->user()->linkedin_email != null)
+                                                <tr>
+                                                    <td>
+                                                        Linkedin Account
+                                                    </td>
+                                                    <td>
+                                                        <a href="/link/linkedin/delete" data-original-title="Delete Linkedin Link" 
+                                                            data-toggle="tooltip" type="button" class="btn btn-sm btn-danger">
+                                                            {{Auth::guard('member')->user()->linkedin_email}} <i class="glyphicon glyphicon-remove"></i> 
+                                                        </a>
+                                                    </td>
+                                                </tr>
+                                            @else
+                                                <tr>
+                                                    <td>
+                                                        Linkedin Account
+                                                    </td>
+                                                    <td>
+                                                        <a href="/link/linkedin" data-toggle="tooltip" type="button" class="btn btn-sm btn-success">
+                                                            Link linkedin account
+                                                        </a>
+                                                    </td>
+                                                </tr>
+                                            @endif
+                                        @endif
+                                    </tbody>
+                                    </table>
+                                </div>
       </div>
     </div>
   </div>
