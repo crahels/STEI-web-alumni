991614c43fee19227f0ba60390b2efeab2cd0b75
stevannohero <13515082@std.stei.itb.ac.id>
2018-04-20 10:17:27 +0700

    show each answer done

--- resources/views/users/qna/showeachanswer.blade.php
+++ resources/views/users/qna/showeachanswer.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.apphome')
 
 @section('content')
-<div class="row create-post-container">
-    <div class="col-3 header-create-post">
-        <h1>Show <br>Answer</h1>
-    </div>
-    <div class="col-8 post">
-        <div class=" col-md-12 col-lg-12">
+<div class="body-qna">
+    <section class="services-section text-center">
+        <div class="section-title">
+            <h3>&nbsp;{{$answer->user->name}}'s Answer </h3>
+        </div>
+    </section>
+    <div class="qna-container">
+        <div class="add-question-container" style="margin: 0px 300px">
             @if($answer->is_pinned == 1)
                 <button data-toggle="tooltip" class="btn pull-right"><i class="fa fa-thumb-tack"></i>&nbsp;&nbsp;PINNED</button> 
             @else
@@ -45,31 +47,17 @@
                     </tr>
                 <tbody>
             </table>
-        </div>
-        <!--<div class="body-article" style="font-size:1.5em;">Answer of Question "{{$answer->question->topic}}: {{$answer->question->body}}"</div>
 
-        <div class="body-article" style="font-size:1.5em;">
-            {{$answer->body}}
-        </div>-->
-    
-        <!--<div class="footer-article">
-            <hr>
-            <small>Written on {{$answer->created_at}}</small><br>
-            <small>Last Editted on {{$answer->updated_at}}</small><br>
-            <small>by {{$answer->user->name}}<span>@if($answer->is_admin == 1) <span>as <span style="color:blue;">admin</span></span>@endif</span></small>
-            <hr>
-        </div>-->
         @if((!Auth::guest() && Auth::user()->IsAdmin == 1) || (Auth::guard('member')->user() != null && $answer->user->id == Auth::guard('member')->user()->id && $answer->is_admin == 0))
-            <a href="/answers/{{$answer->id}}/edit" class="btn btn-warning">
+            <a href="/answers/{{$answer->id}}/edit" class="pull-left btn btn-warning" style="margin-right: 10px">
                 Edit
             </a>
-            {!!Form::open(['action' => ['AnswersController@destroy', $answer->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+            {!!Form::open(['action' => ['AnswersController@destroy', $answer->id], 'method' => 'POST', 'class' => 'pull-left'])!!}
                 {{Form::hidden('_method', 'DELETE')}}
                 {{Form::submit('Delete', ['class' => 'btn btn-danger', 'onclick' => "return confirm('Are you sure you want to delete?')"])}}
             {!!Form::close() !!}
         @endif
-        <!--<br><br><br>
-        <a href="/admin/questions" class="btn btn-info pull-down">&#8592; Back</a>-->
+        </div>
     </div>
 </div>
 @endsection
