81cf98e3106d57dfd4f78b56f2c03c15f62358a0
stevannohero <13515082@std.stei.itb.ac.id>
2018-04-20 10:22:11 +0700

    edit answer done

--- resources/views/users/qna/editanswer.blade.php
+++ resources/views/users/qna/editanswer.blade.php
@@ -3,13 +3,16 @@
 @section('title', 'Edit Answer')
 
 @section('content')
-<div class="row create-post-container">
-    <div class="col-3 header-create-post">
-        <h1>Edit <br>Answer</h1>
-    </div>
-    <div class="col-8 post">
-        {!! Form::open(['action' => ['AnswersController@update', $answer->id], 'method' => 'POST']) !!}
-        <table class="table" style="border-width:0px;">
+<div class="body-qna">
+    <section class="services-section text-center">
+        <div class="section-title">
+            <h3> Edit Answer </h3>
+        </div>
+    </section>
+    <div class="qna-container">
+        <div class="add-question-container" style="margin: 0px 300px">
+            {!! Form::open(['action' => ['AnswersController@update', $answer->id], 'method' => 'POST']) !!}
+            <table class="table" style="border-width:0px;">
             <tbody>
                 <tr>
                     <td>{{Form::label('topic', 'Topic')}}</td>
@@ -20,34 +23,18 @@
                     <td>{{$answer->question->body}}</td>
                 </tr>
                 <tr>
-                    <div class="form-group">
-                            <td>{{Form::label('pin', 'Pin')}}</td>
-                            <td> 
-                                <label class="switch">
-                                    @if ($answer->is_pinned == '1')
-                                        <input name="pin" id="pin" value="yes" type="checkbox" checked>
-                                    @else
-                                        <input name="pin" id="pin" value="yes" type="checkbox">
-                                    @endif
-                                    <span class="slider round"></span>
-                                </label>
-                            </td>
-                    </div>
-                </tr>
-                <tr>
                     <td>{{Form::label('body', "Answer's Body")}}</td>
                     <td>{{Form::textarea('body', $answer->body, ['class' => 'form-control', 'placeholder' => 'Answer'])}}</td>
                 </tr>
             </tbody>
-        </table>
-        {{Form::hidden('_method', 'PUT')}}
-        <div class="bottomButton">
-            {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
-            <a onclick="return confirm('Are you sure you want to leave?')" class="onclick btn btn-danger pull-right" href="/answers/{{$answer->id}}">
-                Cancel
-            </a>
+            </table>
+            {{Form::hidden('_method', 'PUT')}}
+            <div class="bottomButton">
+                {{Form::submit('Submit', ['class' => 'pull-left btn btn-primary'])}}
+                <a onclick="return confirm('Are you sure you want to leave?')" class="onclick btn btn-danger pull-left" href="/answers/{{$answer->id}}" style="margin-left: 20px">Cancel</a>
+            </div>
+            {!! Form::close() !!}
         </div>
-        {!! Form::close() !!}
     </div>
 </div>
 @endsection
