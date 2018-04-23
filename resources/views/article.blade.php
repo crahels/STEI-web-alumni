7365df8394fbdbaad7ff84a832ece1ceba3114bd
stevannohero <13515082@std.stei.itb.ac.id>
2018-04-24 00:08:07 +0700

    edit show article

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -3,53 +3,70 @@
 @section('title', 'Posts')
 
 @section('content')
-<div class="container-fluid">
-  <div class="row">
-    <nav class="col-3 sidebar">
-      <div class="sidebar-sticky">
-        <ul class="nav flex-column">
-        <li class="nav-item" id="nav-two">
-            <a class="nav-link" href="/members">
-              <span id="text-nav-two">
-                <span data-feather="users"></span>
-                <i class="sideMenu">Members</i>
-              </span>    
-            </a>
-          </li>
-          <li class="nav-item" id="nav-one">
-            <a class="nav-link" href="/forum">
-              <span id="text-nav-one">  
-                <span data-feather="home"></span>
-                <i class="sideMenu">Forum</i><span class="sr-only">(current)</span>
-              </span>    
-            </a>
-          </li>
-          <li class="nav-item" id="nav-three">
-            <a class="nav-link" href="/posts">
-              <span id="text-nav-three">
-                <span data-feather="file"></span>
-                <i class="sideMenu">Posts</i>
-              </span>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  <!-- Icons -->
-  <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
-  <script>
-    feather.replace();
-  </script>
+<div class="body-qna">
+    <section class="services-section qna-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-title text-center">
+                        <h3>Welcome to Alumni STEI Forum</h3>
+                        <p>Question & Answer</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="feature-2">
+                        <div class="media">
+                            <div class="pull-left">
+                                <i class="fa fa-book"></i>
+                                <div class="border"></div>
+                            </div>
+                            <div class="media-body">
+                                <h4 class="media-heading">Read Article</h4>
+                                <p>Read all the articles!</p>
+                            </div>
+                        </div>
+                    </div>
+                </div><!-- /.col-md-4 -->
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="feature-2">
+                        <div class="media">
+                            <div class="pull-left">
+                                <i class="fa fa-group"></i>
+                                <div class="border"></div>
+                            </div>
+                            <div class="media-body">
+                                <h4 class="media-heading">Interact</h4>
+                                <p>Interact with the articles</p>
+                            </div>
+                        </div>
+                    </div>
+                </div><!-- /.col-md-4 -->
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="feature-2">
+                        <div class="media">
+                            <div class="pull-left">
+                                <i class="fa fa-comments"></i>
+                                <div class="border"></div>
+                            </div>
+                            <div class="media-body">
+                                <h4 class="media-heading">Share and Go</h4>
+                                <p>Enjoy your time reading all the articles and spread loves</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </section>
 
-    <!-- START Main -->
-    <main role="main" class="col-7">
-          <div class=" section-title text-center">
-              <h3>Welcome to Alumni STEI Article</h3>
-              <p>Get your newest and reliable news about Alumni STEI here</p>
-          </div>
+    <div class="container qna-container">
         @if (count($posts) > 0)
-            @foreach ($posts as $post)
-                <div class="well">
+            <div class="row">
+                @foreach ($posts as $post)
+                <div class="col-2"></div>
+                <div class="well question-container col-8">
                     <div class="row">
                         <div class="col-8 post-card">
                             <h3><a href="/posts/{{$post->id}}">{{$post->title}}</a></h3>
@@ -60,17 +77,25 @@
                         </div>
                     </div>
                 </div>
+                <div class="col-2"></div>
             @endforeach
             <ul class="pagination pull-right">{{$posts->links()}}</ul>
         @else
-            <p>No post</p>
+          <div class="row">
+              <div class="col-2"></div>
+              <div class="well question-container col-8 text-center" style="font-size: 1.5em; font-weight: bolder;">
+                  No Post
+              </div>
+              <div class="col-2"></div>
+          </div>
         @endif
-    </main>
-</div>
-</div>
+      </div>
+  </div>
+
 <script>
     document.getElementById("nav-three").classList.add("active");
     document.getElementById("text-nav-three").classList.add("color-active");
-</script>	
-
+    feather.replace();
+</script>
+<script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
 @endsection
--- resources/views/users/qna/showquestion.blade.php
+++ resources/views/users/qna/showquestion.blade.php
@@ -56,49 +56,7 @@
                             </div>
                         </div>
                     </div>
-                {{-- </div><!-- /.col-md-4 -->
-                <div class="col-md-4 col-sm-6 col-xs-12">
-                    <div class="feature-2">
-                        <div class="media">
-                            <div class="pull-left">
-                                <i class="fa fa-plug"></i>
-                                <div class="border"></div>
-                            </div>
-                            <div class="media-body">
-                                <h4 class="media-heading">Wordpress Plugin</h4>
-                                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu</p>
-                            </div>
-                        </div>
-                    </div>
-                </div><!-- /.col-md-4 -->
-                <div class="col-md-4 col-sm-6 col-xs-12">
-                    <div class="feature-2">
-                        <div class="media">
-                            <div class="pull-left">
-                                <i class="fa fa-joomla"></i>
-                                <div class="border"></div>
-                            </div>
-                            <div class="media-body">
-                                <h4 class="media-heading">Joomla Template</h4>
-                                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu</p>
-                            </div>
-                        </div>
-                    </div>
-                </div><!-- /.col-md-4 -->
-                <div class="col-md-4 col-sm-6 col-xs-12">
-                    <div class="feature-2">
-                        <div class="media">
-                            <div class="pull-left">
-                                <i class="fa fa-cube"></i>
-                                <div class="border"></div>
-                            </div>
-                            <div class="media-body">
-                                <h4 class="media-heading">Joomla Extension</h4>
-                                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu</p>
-                            </div>
-                        </div>
-                    </div>
-                </div><!-- /.col-md-4 --> --}}
+                </div>
             </div><!-- /.row -->
         </div><!-- /.container -->
     </section>
